3e96c22e71bbbdd2a8488c3f33b04ff14ddf302a
Gabriel Artur <gkaiserc@esliceu.net>
2024-05-27 11:05:41 +0200

    Cambios Realizados 27/05/2024 - 11:21

--- contacto.php
+++ contacto.php
@@ -5,7 +5,7 @@
 <main>
     <section><!--contacto-->
             <h3 class="textonormal">Contacto</h3>
-            <form class="formulario" action="insertContact.php">
+            <form class="formulario" action="create_contact.php" method="GET">
                 <fieldset>
                     <legend>Contáctame Rellenando Tus Datos</legend>
                     <div class="contenedor-campos">
