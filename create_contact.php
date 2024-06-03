bc6d58a210664ec79ef9581fd3d9baac1d4c383a
Gabriel Artur <gkaiserc@esliceu.net>
2024-06-03 10:58:46 +0200

    prueba 8 contactos

--- create_contact.php
+++ create_contact.php
@@ -2,19 +2,19 @@
 
 include 'db.php';
 
-if($_SERVER['REQUEST_METHOD']=="POST")
-{
+if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "CREANDO CONTACTO NUEVO <BR>";
     $nombre=$_POST['nombre'];
     $telefono=$_POST['telefono'];
     $correo=$_POST['correo'];
     $mensaje=$_POST['mensaje'];
-
+    
+    
     echo "NOMBRE: ".$nombre."<br>";
     echo "TELEFONO: ".$telefono."<br>";
     echo "CORREO: ".$correo."<br>";
     echo "MENSAJE: ".$mensaje."<br>";
-    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje');";
+    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')";
     echo $sql."<br>";
     $stmt = $conn->prepare($sql);
     $result = $stmt->execute();
