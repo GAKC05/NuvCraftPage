a470af896c48484b708aca59f9c7c1b72b4db63d
Gabriel Artur <gkaiserc@esliceu.net>
2024-06-03 09:49:05 +0200

    porg

--- create_contact.php
+++ create_contact.php
@@ -5,8 +5,6 @@ include 'db.php';
 if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "CREANDO CONTACTO NUEVO <BR>";
     $nombre=$_POST['nombre'];
-    $telefono=$_POST['telefono'];
-    $correo=$_POST['correo'];
     $mensaje=$_POST['mensaje'];
 
     echo "NOMBRE: " .$nombre. "<br>";
