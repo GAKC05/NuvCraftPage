cbd3a26036075c363beac7a796c639081289ff12
Gabriel Artur <gkaiserc@esliceu.net>
2024-06-03 10:40:40 +0200

    prueba 4 contactos

--- create_contact.php
+++ create_contact.php
@@ -2,18 +2,19 @@
 
 include 'db.php';
 
-if($_SERVER['REQUEST_METHOD']=="POST"){
+if($_SERVER['REQUEST_METHOD']=="POST")
+{
     echo "CREANDO CONTACTO NUEVO <BR>";
     $nombre=$_POST['nombre'];
-    $phone=$_POST['telefono'];
-    $email=$_POST['correo'];
+    $telefono=$_POST['telefono'];
+    $correo=$_POST['correo'];
     $mensaje=$_POST['mensaje'];
 
     echo "NOMBRE: ".$nombre."<br>";
-    echo "TELEFONO: ".$phone."<br>";
-    echo "CORREO: ".$email."<br>";
+    echo "TELEFONO: ".$telefono."<br>";
+    echo "CORREO: ".$correo."<br>";
     echo "MENSAJE: ".$mensaje."<br>";
-    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$phone','$email','$mensaje')";
+    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')";
     echo $sql."<br>";
     $stmt = $conn->prepare($sql);
     $result = $stmt->execute();
@@ -24,7 +25,8 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
         echo "ERROR AL CREAR CONTACTO";
     }
 
-}else{
+}
+else{
     echo "ERROR EN METODO POST";
 }
 ?>
