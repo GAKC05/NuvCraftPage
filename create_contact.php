ec789514a182d225b682759d30e9dc32752b54cf
Gabriel Artur <gkaiserc@esliceu.net>
2024-06-03 09:07:38 +0200

    cambios realizados

--- create_contact.php
+++ create_contact.php
@@ -1,7 +1,9 @@
 <?php
 
+include 'db.php';
+
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    echo "CREANDO CONTACTO NUEVO <BR>";
+    echo "CREANDO CONTACTO NUEVO <br>";
     $nombre=$_POST['nombre'];
     $telefono=$_POST['telefono'];
     $correo=$_POST['correo'];
@@ -11,7 +13,13 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "TELEFONO: " .$telefono. "<br>";
     echo "CORREO: " .$correo. "<br>";
     echo "MENSAJE: " .$mensaje. "<br>";
+    $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')";
+    
+    $stmt = $conn->prepare($sql);
+    $result = $ $stmt->execute();
+
 }else{
-    echo "ERROR EN METODO POST"
+    echo "ERROR EN METODO POST";
 }
+
 ?>
