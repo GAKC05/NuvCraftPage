3a5f2fdb25b8a0746b6bca8e4da3afdb60312e65
Gabriel Artur <gkaiserc@esliceu.net>
2024-06-03 09:53:35 +0200

    solucionado?

--- create_contact.php
+++ create_contact.php
@@ -5,12 +5,14 @@ include 'db.php';
 if($_SERVER['REQUEST_METHOD']=="POST"){
     echo "CREANDO CONTACTO NUEVO <BR>";
     $nombre=$_POST['nombre'];
+    $telefono=$_POST['telefono'];
+    $correo=$_POST['correo'];
     $mensaje=$_POST['mensaje'];
 
-    echo "NOMBRE: " .$nombre. "<br>";
-    echo "TELEFONO: " .$telefono. "<br>";
-    echo "CORREO: " .$correo. "<br>";
-    echo "MENSAJE: " .$mensaje. "<br>";
+    echo "NOMBRE: ".$nombre."<br>";
+    echo "TELEFONO: ".$telefono."<br>";
+    echo "CORREO: ".$correo."<br>";
+    echo "MENSAJE: ".$mensaje."<br>";
     $sql="INSERT INTO contactos (nombre,telefono,correo,mensaje) VALUES ('$nombre','$telefono','$correo','$mensaje')";
     echo $sql."<br>";
     $stmt = $conn->prepare($sql);
