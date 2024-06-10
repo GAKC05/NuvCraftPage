4cc3ff0bb2c4930dd2d9f984ae410c80f45383d6
Gabriel Artur <gkaiserc@esliceu.net>
2024-06-10 13:03:41 +0200

    cambios realizados mensajes

--- read.php
+++ read.php
@@ -1,8 +1,31 @@
 <?php
+    include 'db.php';
     include "header.php";
+    $sql = "SELECT id, nombre, telefono, correo, mensaje FROM contactos";
+    $result=$conn->query($sql);
+
+    echo "<pre>";
+    var_dump($result);
+    echo $result->num_rows;
+    echo "</pre>";
 ?>
 
 <h2>Mensajes</h2>
+
+<div class="contenedor listado">
+    <?php
+    while($row = $result->fetch_assoc()){
+        ?>
+            <div class="fila contenedor textonormal">
+                <div class="campo"><?=$row['id']?></div>
+                <div class="campo"><?=$row['nombre']?></div>
+                <div class="campo"><?=$row['telefono']?></div>
+                <div class="campo"><?=$row['correo']?></div>
+                <div class="campo"><?=$row['mensaje']?></div>
+            </div>
+        <?php
+    }
+?>
 
 <?php
     include "footer.php";
